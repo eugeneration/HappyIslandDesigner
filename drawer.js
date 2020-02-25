e947bc90d36216b75fba4633be26af0a898a0f85
Eugene Lee <eugene.lee@Oculus.com>
2020-02-24 23:55:06 -0800

    [draw] drawing with multiple colors

--- drawer.js
+++ drawer.js
@@ -1,37 +1,146 @@
+var colors = {
+    water: '#7cd5c4',
+    sand: '#f0e5a6',
+    level1: '#42753e',
+    level2: '#4ca14e',
+    level3: '#62c360',
+    rock: '#717488',
+}
 
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
-
-
-// Create a circle shaped path with its center at the center
-// of the view and a radius of 30:
-var circle = new Path.Circle({
-    center: view.center,
-    radius: 30,
-    strokeColor: 'black'
-});
+// ===============================================
+// GLOBAL FUNCTIONS
 
 function onResize(event) {
     // Whenever the window is resized, recenter the path:
-    circle.position = view.center;
+    drawBackground();
+    updateSegments();
+    updateColorTools();
 }
-
-// Create a new path once, when the script is executed:
-var myPath = new Path();
-myPath.strokeColor = 'black';
 
 // This function is called whenever the user
 // clicks the mouse in the view:
 function onMouseDown(event) {
-    // Add a segment to the path at the position of the mouse:
-    console.log(project);
-    project.zoom = event.point.x;
+    startDraw(event);
 }
+tool.minDistance = 10;
+function onMouseDrag(event) {
+    draw(event);
+}
+
+function onMouseUp(event) {
+    endDraw(event);
+}
+
+// ===============================================
+// BACKGROUND
+
+var backgroundRect = new Path();
+drawBackground();
+function drawBackground() {
+    // background color
+    backgroundRect.remove();
+    backgroundRect = new Path.Rectangle({
+        point: [0, 0],
+        size: [view.size.width, view.size.height],
+    });
+    backgroundRect.sendToBack();
+    backgroundRect.fillColor = colors.water;
+}
+
+// ===============================================
+// GRID overlay
+var gridCells = 6;
+var grid = [];
+for (var i = 0; i < gridCells; i++) {
+    line = new Path(getSegment(i));
+    line.strokeColor = '#ffffff';
+    line.strokeWidth = 3;
+    line.strokeCap = 'round';
+    line.dashArray = [10, 12];
+    grid.push(line);
+}
+function getSegment(i, horizontal) {
+    var pos = 10 + i * (view.size.width / gridCells); 
+    return [[pos, 10], [pos, 600]]
+}
+function updateSegments() {
+    for (var i = 0; i < gridCells; i++) {
+        var segmentPoints = getSegment(i);
+        grid[i].segments[0].point = segmentPoints[0];
+        grid[i].segments[1].point = segmentPoints[1];
+    }
+}
+
+// ===============================================
+// COLOR CONTROL
+
+var activeColor = Path.Circle([0, 0], 30);
+activeColor.fillColor = paintColor;
+
+function updateColorTools() {
+    activeColor
+}
+function onUpdateColor() {
+    activeColor.fillColor = paintColor;
+}
+
+var paintColor = colors.level1;
+$(document).keydown(function(event) {
+    switch(event.key) {
+        case '1':
+            paintColor = colors.water;    
+            break;
+        case '2':
+            paintColor = colors.sand;    
+            break;
+        case '3':
+            paintColor = colors.level1;    
+            break;
+        case '4':
+            paintColor = colors.level2;    
+            break;
+        case '5':
+            paintColor = colors.level3;    
+            break;
+        case '6':
+            paintColor = colors.rock;    
+            break;
+    }
+    onUpdateColor();
+});
+
+
+// ===============================================
+// DRAWING
+
+// Create a new path once, when the script is executed:
+var myPath;
+function startDraw() {
+    myPath = new Path();
+    myPath.strokeColor = paintColor;
+    myPath.strokeWidth = 10;
+}
+function draw(event) {
+    // Add a segment to the path at the position of the mouse:
+    myPath.add(event.point);
+    myPath.smooth({type: 'catmull-rom'});
+}
+function endDraw() {
+
+}
+
+
+// ===============================================
+// COORDINATE HELPERS
+
+function canvasCoordinate(values) {
+    if (values.top) {
+
+    }
+    else if (values.bottom) {
+
+    }
+}
+
+
+
---
+++ paper-zoom.js
@@ -0,0 +1,119 @@
+// ===============================================
+// GLOBAL FUNCTIONS
+
+var factor = 1.05;
+var _minZoom;
+var _maxZoom;
+var mouseNativeStart;
+var viewCenterStart;
+
+$(view.element).mousewheel(function(event) {
+    var mousePosition = new Point(event.offsetX, event.offsetY);
+    changeZoomCentered(event.deltaY, mousePosition);
+});
+
+function onResize(event) {
+    // Whenever the window is resized, recenter the path:
+}
+
+// This function is called whenever the user
+// clicks the mouse in the view:
+function onMouseDown(event) {
+    viewCenterStart = view.center;
+    // Have to use native mouse offset, because ev.delta 
+    //  changes as the view is scrolled.
+    mouseNativeStart = new Point(ev.event.offsetX, ev.event.offsetY);
+}
+
+tool.minDistance = 10;
+function onMouseDrag(event) {
+    if(viewCenterStart) {   
+        var nativeDelta = new Point(
+            event.offsetX - mouseNativeStart.x,
+            event.offsetY - mouseNativeStart.y
+        );
+        // Move into view coordinates to subract delta,
+        //  then back into project coords.
+        view.center = view.viewToProject( 
+            view.projectToView(viewCenterStart)
+            .subtract(nativeDelta));
+    }
+}
+
+function onMouseUp(event) {
+    if(mouseNativeStart){
+        mouseNativeStart = null;
+        viewCenterStart = null;
+    }
+}
+
+
+// ===============================================
+// PUBLIC FUNCTIONS
+
+setZoomRange([view.size, new Size(100, 100)]);
+
+function setZoomRange(range /*paper.Size[]*/) /* number[] */ {
+    var view = project.view;
+    var aSize = range.shift();
+    var bSize = range.shift();
+    var a = aSize && Math.min( 
+        view.bounds.height / aSize.height,         
+        view.bounds.width / aSize.width);
+    var b = bSize && Math.min( 
+        view.bounds.height / bSize.height,         
+        view.bounds.width / bSize.width);
+    var min = Math.min(a,b);
+    if(min){
+        _minZoom = min;
+    }
+    var max = Math.max(a,b);
+    if(max){
+        _maxZoom = max;
+    }
+    return [_minZoom, _maxZoom];
+}
+
+// ===============================================
+// ZOOM FUNCTIONS
+
+function setZoomConstrained(zoom) {
+    if(_minZoom) {
+        zoom = Math.max(zoom, _minZoom);
+    }
+    if(_maxZoom){
+        zoom = Math.min(zoom, _maxZoom);
+    }
+    var view = project.view;
+    if(zoom != view.zoom){
+        view.zoom = zoom;
+        return zoom;
+    }
+    return null;
+}
+
+function changeZoomCentered(delta, mousePos) {
+    if (!delta) {
+        return;
+    }
+    var view = project.view;
+    var oldZoom = view.zoom;
+    var oldCenter = view.center;
+    var viewPos = view.viewToProject(mousePos);
+    
+    var newZoom = delta > 0
+        ? view.zoom * factor
+        : view.zoom / factor;
+    newZoom = setZoomConstrained(newZoom);
+    
+    if(!newZoom){
+        return;
+    }
+
+    var zoomScale = oldZoom / newZoom;
+    var centerAdjust = viewPos.subtract(oldCenter);
+    var offset = viewPos.subtract(centerAdjust.multiply(zoomScale))
+        .subtract(oldCenter);
+
+    view.center = view.center.add(offset);
+};
