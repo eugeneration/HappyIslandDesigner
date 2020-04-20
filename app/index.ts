b0b6f6a6f715d815410bf5202d7f72b703bead2b
Eugene Lee <eugene.lee@Oculus.com>
2020-04-19 23:22:25 -0700

    [lib] add browser-update. tested by changing chrome version to 81 and saw the notif

--- app/index.ts
+++ app/index.ts
@@ -2,14 +2,14 @@ import { install } from './install';
 import { multitouch } from './paper-multitouch';
 import { zoom } from './paper-zoom';
 import { drawer } from './drawer';
+import browserUpdate from 'browser-update';
 import './index.scss';
+
+browserUpdate({required:{i:79,f:45,o:45,s:-2,c:60},insecure:true,api:2020.03});
 
 (async () => {
   await install();
-
   multitouch();
-
   drawer();
-
   zoom();
 })();
