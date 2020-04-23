8e684cbb6c9a7a1e224375942a5ce9c478a9fc80
Pupu(DotNet) <pupu721+github@gmail.com>
2020-04-23 17:55:51 +0800

    Add language support for zh-TW ,Chinese (Taiwan)

--- data/locale.js
+++ data/locale.js
@@ -111,6 +111,62 @@ const locale = {
             twitter: '推特'
         }
     },
+    'zh-TW': {
+        translation: {
+            hotkey: '快速鍵',
+            hotkey_tips1: '空白鍵 + 拖曳\n'+
+                'alt + 滾輪\n'+
+                '\\\n'+
+                'shift + 拖曳\n'+
+                '[ ]\n'+
+                'p\n'+
+                'alt + 單點\n'+
+                'delete\n'+
+                'ctrl + z\n'+
+                'ctrl + y\n'+
+                '\n'+
+                'v\n'+
+                'b\n'+
+                'n\n'+
+                'm\n'+
+                '\n'+
+                'ctrl + s\n'+
+                'ctrl + o\n'+
+                'esc\n'+
+                '?\n'+
+                '',
+            hotkey_tips2: '移動\n'+
+                '放大縮小\n'+
+                '打開/關閉格線\n'+
+                '畫線\n'+
+                '調整筆刷大小\n'+
+                '方形/圓形筆刷\n'+
+                '顏色選擇\n'+
+                '刪除被選取的內容\n'+
+                '復原\n'+
+                '取消復原\n'+
+                '\n'+
+                '地形工具 \n'+
+                '路徑工具\n'+
+                '房屋工具\n'+
+                '建築工具\n'+
+                '\n'+
+                '存檔\n'+
+                '打開地圖檔案\n'+
+                '主選單\n'+
+                '快速鍵\n'+
+                '',
+            mainmenu: '主選單',
+            save_image: '存成圖片',
+            load_map: '載入地圖',
+            new_map: '建立新的地圖',
+            clear_warn: '確定要清除地圖嗎？您未儲存的內容將會消失',
+            load_screenshot: '載入地圖',
+            load_game_map: '載入遊戲地圖',
+            load_game_map_instructions: '1. 上傳地圖的照片或螢幕截圖\n\n2. 標註格線的四個角落',
+            twitter: 'Twitter'
+        }
+    },
     'es-ES': {
         translation: {
             hotkey: 'Atajos de teclado',
