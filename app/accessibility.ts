86c2f26a0d078d578e6246994e24050088b66f99
kit-repo-depot <51519580+kit-repo-depot@users.noreply.github.com>
2020-05-20 18:18:57 -0400

    Create accessibility.ts

---
+++ app/accessibility.ts
@@ -0,0 +1,6 @@
+// Where we put accessibility stuff!
+export var accessibleBrush:boolean = false;
+
+export function accessibleBrushSetter() {
+    accessibleBrush = accessibleBrush ? false : true;
+}
